ecefba475c4e7241061a899417755e3c925f331a
henrique-maceira <henrique.s.maceira@gmail.com>
2020-08-10 23:44:07 -0300

    update aula 15
    primeira parte da finalização dos exercícios do mundo 2 de python 3.

--- pacote download/aula15.py
+++ pacote download/aula15.py
@@ -1,5 +1,5 @@
 import random
-'''
+
 #===exercício01===
 c = soma = 0
 while True:
@@ -56,8 +56,47 @@ while True:
     if(resp == "N"):
         print(f"O número de pessoas com mais de 18 anos é de {conti}.\nO número de homens cadastrados é de {conth}.\nO número de mulheres cadastradas com menos de 20 anos é de {contm}.")
         break
-'''
-#===exercício05===
 
-    
+#===exercício05===
+mil = soma = produto = barato = 0
+barato = 5000000000
+name = str
+while True:
+        prod = str(input("Insira o nome do produto: "))
+        val = float(input("Insira o preço do produto: "))
+        soma += val
+        if (val > 1000):
+            mil += 1
+        if (val < barato):
+            barato = val
+            name = prod
+        resp = str(input("Deseja inserir outro produto? [S/N]: ")).upper()
+        if (resp == "N"):
+            print(f"Você gastou R$ {soma}.\n{mil} produtos custam mais que mil reais e o item mais barato é o (a): {name}.")
+            break
+
+#===exercício06=== *A SER FINALIZADO*
+op1 = cont2 = cont3 = cont4 = cont5 = 0
+while True:
+    sac = int(input("Insira o valor a ser sacado: "))
+    if((sac/50)%2 == 0):
+        op1 = (sac/50)
+        print(f"Você receberá {op1} notas de R$50.")
+        break
+    else:
+        while ((sac - 50)>50):
+            sac = sac - 50
+            cont2 += 1
+        while ((sac - 20)<50 and (sac - 20)>=20):
+            sac = sac - 20
+            cont3 += 1
+        while ((sac - 10)<20 and (sac - 10)>=10):
+            sac = sac - 10
+            cont4 += 1
+        while ((sac - 1)<10):
+            sac = sac - 1 
+            cont5 += 1
+        print(f"Você receberá {cont2} notas de R$50.\n{cont3} notas de R$20.\n{cont4} notas de R$10.\n{cont5} notas de R$1,00.")
+        break
+        
     
